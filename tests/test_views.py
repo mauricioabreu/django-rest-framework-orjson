ec78ad73efb13341c6713c8eacaa1f789aa3242d
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2019-07-17 14:10:20 +0700

    Fix: Forgot to import codecs
    Complement a test case

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,16 @@
 import pytest
 from rest_framework.reverse import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
 
 from .models import Game
 
 pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 
 def test_detail(client):
@@ -29,9 +35,10 @@ def test_list(client):
     assert json[0]['name'] == 'The Legend of Zelda: Ocarina of Time'
 
 
-def test_create(client):
+def test_create(api_client):
     url = reverse('game-list')
     data = {
         'name': 'The Legend of Zelda: Ocarina of Time',
     }
-    response = client.post(url, data, format='json')
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
